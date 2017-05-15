414b4774af3e32498c6e4c7f2c3ddc65b46c9a87
Dennis Siaril <Dennis.siaril@gmail.com>
2017-05-15 15:52:37 +1000

    Update PercentageAI.java

--- RTP - Stock Market Simulator/src/client/PercentageAI.java
+++ RTP - Stock Market Simulator/src/client/PercentageAI.java
@@ -17,7 +17,7 @@ public class PercentageAI implements Trader
 		double result = 0.0;
 		for (int i = 0; i < indexList.size(); i++)
 		{
-			result += stockList[indexList[i]].getPrice();
+			result += stockList.get(indexList.get(i)).getPrice();
 		}
 		return result;
 	}
@@ -29,10 +29,10 @@ public class PercentageAI implements Trader
 		//sort which stocks are eligible
 		for (int i = 0; i < stockList.size(); i++)
 		{
-			if (stockList[i].getPrice() < initialStocks.getPrice() * 0.8) //change percentages here
+			if (stockList.get(i).getPrice() < initialStocks.getPrice() * 0.8) //change percentages here
 			{
 				result.add(i);
-				percentages.add(calculatePercentage(initialStocks[i].getPrice(), stockList[i].getPrice()));
+				percentages.add(calculatePercentage(initialStocks.get(i).getPrice(), stockList.get(i).getPrice()));
 			}
 		}
 		//check to see if everything can be afforded
@@ -46,10 +46,10 @@ public class PercentageAI implements Trader
 			for (int i = 0; i < sortedPercentages.size(); i++)
 			{
 				//Oh my goooooooood
-				if (stockList[result[percentages.indexOf(sortedPercentages[i])]].getPrice() > copyBalance)
-					copyResult.remove(percentages.indexOf(sortedPercentages[i]));
+				if (stockList.get(result.get(percentages.indexOf(sortedPercentages.get(i)))).getPrice() > copyBalance)
+					copyResult.remove(percentages.indexOf(sortedPercentages.get(i)));
 				else
-					copyBalance -= stockList[result[percentages.indexOf(sortedPercentages[i])]].getPrice();
+					copyBalance -= stockList.get(result.get(percentages.indexOf(sortedPercentages.get(i)))).getPrice();
 			}
 			result = copyResult;
 		}
@@ -61,8 +61,8 @@ public class PercentageAI implements Trader
 		LinkedList<int> result = new LinkedList<int>();
 		for (int i = 0; i < stockList.size(); i++)
 		{
-			if ((stockList[i].getPrice() > initialStocks.getPrice() * 1.2) || (stockList[i].getPrice() > initialStocks.getPrice() * 0.9))
-				result.add(stockList[i]);
+			if ((stockList.get(i).getPrice() > initialStocks.getPrice() * 1.2) || (stockList.get(i).getPrice() > initialStocks.getPrice() * 0.9))
+				result.add(stockList.get(i));
 		}
 		return result; //if empty don't sell
 	}
@@ -106,11 +106,12 @@ public class PercentageAI implements Trader
 				//add to portfolio and update stock history for next decision
 				for (int i = 0; i < stocksToBuy.size(); i++)
 				{
-					initialStocks[i] = stockList[i];
-					portfolio.add(stockList[i]);
-					ownedStock.add(stockList[i].getName());
-					balance -= stockList[i].getPrice();
+					initialStocks.get(i) = stockList.get(i);
+					portfolio.add(stockList.get(i));
+					ownedStock.add(stockList.get(i).getName());
+					balance -= stockList.get(i).getPrice();
 				}
+				//write to log
 			}
 		}
     }
@@ -131,11 +132,12 @@ public class PercentageAI implements Trader
 				//add to portfolio and update stock history for next decision
 				for (int i = 0; i < stocksToBuy.size(); i++)
 				{
-					initialStocks[i] = stockList[i];
-					portfolio.remove(stockList[i]);
-					ownedStock.remove(stockList[i].getName());
-					balance += stockList[i].getPrice();
+					portfolio.remove(stockList.get(i));
+					ownedStock.remove(stockList.get(i).getName());
+					balance += stockList.get(i).getPrice();
 				}
+				for (int i = 0; i < stockList.size(); i++)
+					initialStocks.get(i) = stockList.get(i);
 			}
 		}
 }
