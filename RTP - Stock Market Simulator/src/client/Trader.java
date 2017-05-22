5202fb2e946c524ea5107a7dba897a6cda784d13
David <dgrani@hotmail.com>
2017-05-22 16:06:26 +1000

    Implemented communication between market and traders

--- RTP - Stock Market Simulator/src/client/Trader.java
+++ RTP - Stock Market Simulator/src/client/Trader.java
@@ -1,32 +1,38 @@
 package client;
 
 import java.io.Serializable;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import server.Stock;
 
 public abstract class Trader {
 	protected static int balance;
-	protected static LinkedList<Stock> portfolio;
+	protected static ArrayList<Stock> portfolio;
+	protected static ArrayList<Stock> marketStocks;
+
 	protected static LinkedList<String> ownedStock; //used for quick finding stocks bought by name
 	
 	protected static InetAddress marketIPAdress;
-//	protected static DatagramSocket socket;
-	protected static ConnectionToServer server;
 	protected static LinkedBlockingQueue<Object> messages;
 	protected static Socket socket;
 	
@@ -34,149 +40,83 @@ public abstract class Trader {
 	
 	public abstract void sellStock(LinkedList<Stock> stockList);
 	
-//	public static Stock deserialize(byte[] data) throws IOException, ClassNotFoundException {
-		
-		//ByteArrayInputStream in = new ByteArrayInputStream(data);
-		//ObjectInputStream is = new ObjectInputStream(in);
-		//return is.readObject();
-//		Stock desStock = (Stock) SerializationUtils.deserialize(data);
-//		return desStock;
-//	}
-	
+	protected abstract void analyzeMarketForDecision();
+
 	public Trader() throws UnknownHostException, IOException{
 		System.out.println("Trader launched");
 		int portNumber = 4003;
 		String ipAddress = "127.0.0.1";
 		socket = new Socket(ipAddress, portNumber);
-		messages = new LinkedBlockingQueue<Object>();
-		System.out.println("2");
-		server = new ConnectionToServer(socket);
-		System.out.println("1");
 
-		Thread messageHandling = new Thread() {
-            public void run(){
-                while(true){
-                    try{
-                        Object message = messages.take();
-                        // Do some handling here...
-                        System.out.println("Message Received: " + message);
-                    }
-                    catch(InterruptedException e){ }
-                }
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String fromServer;
+            fromServer = in.readLine();
+            String initialStocksAndPricesList = fromServer;
+            marketStocks = parseStocksAndPricesString(initialStocksAndPricesList);
+            
+            while (true){
+            	analyzeMarketForDecision();
+            	printCurrentLocalStockPrices();
+            	
+            	fromServer = in.readLine();
+            	String updatedPrices = fromServer;
+            	List<String> newPriceList = parsePricesString(updatedPrices);
+            	updateLocalPrices(newPriceList);
             }
-        };
-        messageHandling.setDaemon(true);
-        messageHandling.start();
+	
+	}
+
+	/*
+	FileWriter fw = null;
+	BufferedWriter bw = null;
+	// Get current working directory and set up log
+		String dir = Paths.get(".").toAbsolutePath().normalize().toString();
+		String logDir = dir + "\\TraderLog.txt";
+		File traderLog = new File(logDir);
+		fw = new FileWriter(traderLog);
+		bw = new BufferedWriter(fw);
+	*/
+	private void printCurrentLocalStockPrices() {
+		for(Stock stock : marketStocks)
+		{
+			System.out.println(stock.getName() + " - $" + stock.getPrice());
+		}
+		System.out.println("\n");
+	}
+
+	public List<String> parsePricesString(String updatedPrices) {
+		//separate the string with zero or more whitespace a comma and then zero or more whitespace
+		List<String> updatedPricesList = Arrays.asList(updatedPrices.split("\\s*,\\s*"));
+		return updatedPricesList;
 	}
 	
-	public static void main(String[] args) throws Exception
-	{	
-		
-        /*
-		
-		FileWriter fw = null;
-		BufferedWriter bw = null;
-		try {
-			//Set up connection to Stock Market
-			socket = new DatagramSocket(4003);
-			marketIPAdress = InetAddress.getByName("127.0.0.1"); //change IP address
-			
-			//initialize class variables
-			balance = 0;
-			portfolio = new LinkedList<Stock>();
-			
-			// Get current working directory and set up log
-			String dir = Paths.get(".").toAbsolutePath().normalize().toString();
-			String logDir = dir + "\\TraderLog.txt";
-			File traderLog = new File(logDir);
-			fw = new FileWriter(traderLog);
-			bw = new BufferedWriter(fw);
-			
-			//tell server (stock market) that trader has gone online
-			
-			
-			while (true)
-			{
-				byte[] receiveData = new byte[1024];
-				
-				// Recieve a packet
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                try
-                {
-                    //DatagramSocket receiverSocket;
-					socket.receive(receivePacket);
-                }
-                catch (IOException e)
-                {
-                    System.out.println(e.getMessage());
-                }
-                
-				LinkedList<Stock> stockList = new LinkedList<Stock>();
-				stockList.add(deserialize(receivePacket.getData()));
-				
-				buyStock(stockList);
-				sellStock(stockList);
-					
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (bw != null)
-					bw.close();
-
-				if (fw != null)
-					fw.close();
-
-			} catch (IOException ex) {
-
-				ex.printStackTrace();
-
-			}
-		}*/
+	public void updateLocalPrices(List<String> updatedPricesList)
+	{
+		for(int i = 1; i<=updatedPricesList.size(); i++)
+		{
+			marketStocks.get(i-1).updatePrice(Double.parseDouble(updatedPricesList.get(i-1)));
+		}
 	}
-	
-	private class ConnectionToServer {
-        ObjectInputStream in;
-        ObjectOutputStream out;
-        Socket socket;
+	/**
+	 * Example input: "name1,price1,name2,price2"
+	 * @param initialStocksAndPricesList
+	 * @return
+	 */
+	public ArrayList<Stock> parseStocksAndPricesString(String initialStocksAndPricesList) {
+		//separate the string with zero or more whitespace a comma and then zero or more whitespace
+		List<String> stockNamesAndPrices = Arrays.asList(initialStocksAndPricesList.split("\\s*,\\s*"));
+		ArrayList<Stock> stockList = new ArrayList<Stock>();
+		int i = 1;
+		while(i<stockNamesAndPrices.size())
+		{
+			Stock stock = new Stock(stockNamesAndPrices.get(i-1), Double.parseDouble(stockNamesAndPrices.get(i)));
+			i += 2;//add 2 since one stock takes up two spaces (one for name, one for price)
+			stockList.add(stock);
+		}
+		return stockList;
+	}
 
-        ConnectionToServer(Socket sock) throws IOException {
-            this.socket = sock;
-            System.out.println("12");
-            in = new ObjectInputStream(socket.getInputStream());
-            System.out.println("1234");
-            out = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("141");
+	public abstract void main(String[] args) throws Exception;
 
-            Thread read = new Thread(){
-                public void run(){
-                    while(true){
-                        try{
-                            Object obj = in.readObject();
-                            messages.put(obj);
-                        }
-                        catch(IOException | ClassNotFoundException | InterruptedException e){ e.printStackTrace(); }
-                    }
-                }
-            };
-            System.out.println("6");
-
-            read.setDaemon(true);
-            read.start();
-        }
-
-        private void write(Object obj) {
-            try{
-                out.writeObject(obj);
-            }
-            catch(IOException e){ e.printStackTrace(); }
-        }
-
-
-    }
-
-    public void send(Object obj) {
-        server.write(obj);
-    }
 }
--- RTP - Stock Market Simulator/src/server/StockMarket.java
+++ RTP - Stock Market Simulator/src/server/StockMarket.java
@@ -1,7 +1,9 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -11,6 +13,7 @@ import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -20,11 +23,9 @@ import client.Trader;
 public class StockMarket {
 	private  ArrayList<Stock> stockList = new ArrayList<Stock>();
 	
-	private  ArrayList<ConnectionToClient> clientList;
-    private  LinkedBlockingQueue<Object> messages;
+	private ArrayList<Socket> clientSocketList = new ArrayList<Socket>();
     private  ServerSocket serverSocket;
-    
-    
+        
 	private  double positiveBias = 0.51;//0-1, higher for more positive
 	private  double maxPercentageChange = 0.05;
 	private  long minimumTimeIntervalBetweenUpdates = ((long) 10000000000.);//nano seconds ie. 10 seconds
@@ -40,114 +41,101 @@ public class StockMarket {
 		System.out.println("StockMarket Launched");
 		
 		int portNumber = 4003;
-		long lastUpdateTime = System.nanoTime();
-		long currentTime = System.nanoTime();
-		
-		clientList = new ArrayList<ConnectionToClient>();
-        messages = new LinkedBlockingQueue<Object>();
-        serverSocket = new ServerSocket(portNumber);
+		serverSocket = new ServerSocket(portNumber);
         
-        
-        Thread accept = new Thread() {
-            public void run(){
+        Thread updater = new Thread(){
+        	public void run(){
+        		long lastUpdateTime = System.nanoTime();
+        		long currentTime = System.nanoTime();
                 while(true){
-                    try{
-                        Socket s = serverSocket.accept();
-                        clientList.add(new ConnectionToClient(s));
-                        System.out.println("added client");
-                    }
-                    catch(IOException e){ e.printStackTrace(); }
+     			
+        			currentTime = System.nanoTime();
+        			if(currentTime - lastUpdateTime > minimumTimeIntervalBetweenUpdates){
+        				lastUpdateTime = currentTime;
+        				updateMarket();
+//        				System.out.println("updated market");
+        				notifyClientsOfPriceChange();
+        			}
                 }
-            }
+        	}
         };
-        
-        accept.setDaemon(true);
-        accept.start();
-                
-        Thread messageHandling = new Thread() {
-            public void run(){
-                while(true){
-                    try{
-                        Object message = messages.take();
-                        // Do some handling here...
-                        System.out.println("Message Received: " + message);
-                    }
-                    catch(InterruptedException e){ }
-                }
-            }
-        };
-        
-        messageHandling.setDaemon(true);
-        messageHandling.start();
-        
+        updater.start();
         
         while(true){
-			//read and handle messages from traders -> use separate threads?
-//			socket.receive(packet);
-//			handlePacket(packet, buffer);
-			
-			currentTime = System.nanoTime();
-			if(currentTime - lastUpdateTime > minimumTimeIntervalBetweenUpdates){
-				lastUpdateTime = currentTime;
-				updateMarket();
-				System.out.println("updated market");
-				notifyClientsOfPriceChange();
-			}
+        	System.out.println("waiting to accept");
+        	Socket socket = serverSocket.accept();
+        	System.out.println("accepted");
+        	clientSocketList.add(socket);
+        	
+        	Thread accepter = new Thread(){
+        		public void run(){
+        			DataOutputStream out = null;
+        			Socket sock = socket;
+        			try {
+        				out = new DataOutputStream(sock.getOutputStream());
+        		        } catch (IOException e) {
+        		            return;
+        		        }
+        		        String message = "";
+    		            try {
+    		            	//Send a message on accept to the client detailing the stock names and prices
+    		        		for(Stock stock : stockList){
+    		        			message += stock.getName() + "," + stock.getPrice() + ",";
+    		        		}
+    		        		//remove last comma
+    		        		message = message.substring(0,message.length()-1);
+    		        		//add a new line
+    		        		message += "\n";
+    		        		out.writeBytes(message);
+    		        		out.flush();
+    		            } catch (IOException e) {
+    		                e.printStackTrace();
+    		                return;
+    		            }
+        		}
+        	};
+        	accepter.start();
         }
 	}
 	
 	public static void main(String[] args) throws IOException{
-
-
 		StockMarket stockMarket = new StockMarket();
-		
-		//TODO handle exceptions with try catch don't just throw
-		
-        
-        
-//		byte[] buffer = new byte[2048];
-//        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-         
-		
-		
-		//open connection to allow traders to connect
-//		DatagramSocket socket = new DatagramSocket(portNumber);
-
-		
-	}
-	
-	
-	
-	
-	
-	/**
-	 * Handle a packet from a client,
-	 * we'll need to be able to do things like:
-	 * register - When a client comes online for the first time, they'll register with the server so the server knows where to send updates to.
-	 * buy -
-	 * sell
-	 * @param packet
-	 * @param buffer
-	 */
-	private static void handlePacket(DatagramPacket packet, byte[] buffer) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	/**
+	 * No need for stock names as prices are always in the same order
 	 * Format of (String) message:
-	 * "stockName1,priceInt,stockName2,priceInt2,stockName3,priceInt3,"
+	 * "priceDouble,priceDouble2,priceDouble3"
 	 */
 	private void notifyClientsOfPriceChange() {
 		String message = "";
 		for(Stock stock : stockList){
-			message += stock.getName() + "," + stock.getPrice() + ",";
+			message += stock.getPrice() + ",";
 		}
 		//remove last comma
 		message = message.substring(0,message.length()-1);
-		sendToAll(message);		
+		sendToAllClients(message);		
 	}
 
+    public void sendToAllClients(String message){
+        for(Socket clientSocket : clientSocketList)
+            write(message, clientSocket);
+    }
+    
+    public void write(String message, Socket socket){
+    	//messages need to end with a new line, so add it here
+    	message += "\n";
+    	
+    	DataOutputStream out;
+		try {
+			out = new DataOutputStream(socket.getOutputStream());
+			out.writeBytes(message);
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
 	public void addStock(Stock stock){
 		stockList.add(stock);
 	}
@@ -174,53 +162,6 @@ public class StockMarket {
 			double newPrice = currentPrice + priceChange;
 			
 			stock.updatePrice(newPrice);
-		}
-	}
-	
-    public void sendToOne(int index, Object message)throws IndexOutOfBoundsException {
-        clientList.get(index).write(message);
-    }
-
-    public void sendToAll(Object message){
-        for(ConnectionToClient client : clientList)
-            client.write(message);
-    }
-	
-    //can this be static???
-	private class ConnectionToClient {
-		ObjectInputStream in;
-		ObjectOutputStream out;
-		Socket socket;
-		
-		ConnectionToClient(Socket sock) throws IOException {
-			this.socket = sock;
-			System.out.println("1");
-			in = new ObjectInputStream(socket.getInputStream());
-			System.out.println("2");
-			out = new ObjectOutputStream(socket.getOutputStream());
-			
-			Thread read = new Thread(){
-				public void run(){
-					while(true){
-						try{
-							Object obj = in.readObject();
-							messages.put(obj);
-						}
-						catch(IOException | ClassNotFoundException | InterruptedException e){ 
-							e.printStackTrace(); 
-						}
-					}
-				}
-			};
-			
-			read.setDaemon(true); // terminate when main ends
-			read.start();
-		}
-		public void write(Object obj) {
-			try{
-				out.writeObject(obj);
-			}
-			catch(IOException e){ e.printStackTrace(); }
 		}
 	}
 }
